bb4aeb45b4bcbcf5609dc5ca0650bd18a1ab341b
Devan <devanmoller51@gmail.com>
2020-11-19 20:44:01 +0200

    Frontend Tuning

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,18 +1,31 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const express = require('express');
+class App extends React.Component {
+  getReq() {
+    console.log("Requesting data from API")
+    var request = new XMLHttpRequest();
+    request.open('GET','/post', true);
+    request.onload = function () {
+      var data = JSON.parse(this.response);
+      console.log(data);
+    }
+  }
 
-const app = express();
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h3>Login</h3>
+        </header>
+        <div className="content-container">
+          <div className="btn" onClick={this.getReq}>Get</div> 
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
